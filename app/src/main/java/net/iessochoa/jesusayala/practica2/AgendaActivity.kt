933726cd8a928abfd570d745774893336d6716bb
JesusAyala <jesusayalamilan2@gmail.com>
2022-11-06 22:47:01 +0100

    Final de la práctica

--- app/src/main/java/net/iessochoa/jesusayala/practica2/AgendaActivity.kt
+++ app/src/main/java/net/iessochoa/jesusayala/practica2/AgendaActivity.kt
@@ -1,11 +1,39 @@
 package net.iessochoa.jesusayala.practica2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class AgendaActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agenda)
+
+        val btAceptar = findViewById<Button>(R.id.btAceptar)
+        val btCancelar = findViewById<Button>(R.id.btCancelar)
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val etApellidos = findViewById<EditText>(R.id.etApellidos)
+        var tvNuevoContacto = findViewById<TextView>(R.id.tvNuevoContacto)
+
+        btAceptar.setOnClickListener {
+            if (!etNombre.text.isEmpty() && !etApellidos.text.isEmpty()){
+
+                val nombreApellidos = "${etNombre.text}  ${etApellidos.text}"
+                tvNuevoContacto.text = nombreApellidos
+
+            }else {
+                Toast.makeText(this, "Introduce nombre y apellidos",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btCancelar.setOnClickListener{
+            btCancelar.setOnClickListener() { finish() }
+        }
     }
 }
--- app/src/main/java/net/iessochoa/jesusayala/practica2/MainActivity.kt
+++ app/src/main/java/net/iessochoa/jesusayala/practica2/MainActivity.kt
@@ -1,11 +1,26 @@
 package net.iessochoa.jesusayala.practica2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btAgenda = findViewById<Button>(R.id.btAgenda)
+        btAgenda.setOnClickListener {
+            val intent = Intent(this,AgendaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btReceta = findViewById<Button>(R.id.btReceta)
+        btReceta.setOnClickListener {
+            val intent = Intent(this,RecetaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
